9373d01f09813e78280b7f66d0e4867f79062563
Vipul Sharma <sharmavipul@gmail.com>
2011-03-01 22:46:34 -0800

    adding new changes Product classes, list, better parsing

---
+++ src/main/java/com/lumlate/midas/meta/Product.java
@@ -0,0 +1,32 @@
+package com.lumlate.midas.meta;
+
+public class Product {
+	private String department;   //electronics
+	private String producttype;  //headphone
+	private String product;      //bose-xyz headphone
+	private String manufacturer;
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getProducttype() {
+		return producttype;
+	}
+	public void setProducttype(String producttype) {
+		this.producttype = producttype;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+}
